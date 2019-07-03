859b9fa5ca2a4bb56ba285b337065fe0414647ee
Gustavo Palmeira de Azevedo <gustavo.azevedo@webmotors.com.br>
2019-07-02 22:20:03 -0300

    Installed normalize.css and emotion css for styling components

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const StyledHeader = styled.header`
+	width: 100%;
+	height: auto;
+	position: relative;
+	margin: 0 auto;
+	background-color: #f00;
+`;
 
 function Header({ children }) {
 	return (
-		<header>
+		<StyledHeader>
 			Header
-		</header> 
+		</StyledHeader> 
 	);
 }
 
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,17 +1,68 @@
-import React from 'react';
+import React, { Fragment } from "react";
+import { css, Global } from "@emotion/core";
+import styled from "@emotion/styled";
 
 /** COMPONENTS */
-import Header from './../Header';
-import Footer from './../Footer';
-/** COMPONENTS */
+import Header from "./../Header";
+import Footer from "./../Footer";
+/** END COMPONENTS */
 
-function Layout() {
+/** NORMALIZE.CSS */
+import "normalize.css";
+/** END NORMALIZE.CSS */
+
+/** GLOBAL STYLES */
+const GlobalStyles = css`
+	:root {
+		--default-font-family: "Montserrat", sans-serif;
+	}
+
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+
+		&:before,
+		&:after {
+			box-sizing: border-box;
+		}
+	}
+
+	html {
+		font-size: 10px;
+		-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+	}
+
+	body {
+		text-rendering: optimizelegibility;
+		font-family: var(--default-font-family);
+		color: #000;
+	}
+`;
+/** END GLOBAL STYLES */
+
+/** STYLED */
+const StyledLayout = styled.div`
+	width: 100%;
+	min-height: 100vh;
+`;
+
+const StyledMain = styled.main`
+	width: 100%;
+	min-height: 800px;
+`;
+/** END STYLED */
+
+function Layout({ children }) {
 	return (
-		<div>
-			<Header />
-			
-			<Footer />
-		</div> 
+		<Fragment>
+			<Global styles={GlobalStyles} />
+			<StyledLayout>
+				<Header />
+				<StyledMain>{children}</StyledMain>
+				<Footer />
+			</StyledLayout>
+		</Fragment>
 	);
 }
 
